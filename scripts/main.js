0ca3b6cb0242d0e2f4fd109c665d378b07cfa3b2
Sam Odell <7421902@isd535.org>
2022-02-07 10:29:45 -0600

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,34 @@
-// Put your JavaScript here
+//id from index.html
+let canvas = document.getElementById("Lawton");
+let ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50. 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+//shopping cart info
+let defHeight = 50;
+let defWidth = 50;
+let defX = (canvas.width - defWidth) / 2;
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+
+//draw multi vitamin
+function drawDefender() {
+    ctx.beginPath();
+    ctx.rect(defX, canvas.height - defHeight, defWidth, defHeight);
+    ctx.fillStyle = "0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+ 
+//creates objects
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    drawDefender();
+}
